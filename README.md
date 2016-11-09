cbd8c728ed96b96e625e272ce31c712077eb6808
Rob Drew <rdrew@upei.ca>
2016-11-09 15:37:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# remove_islandora_metadata_fieldset
---
+++ js/remove_metadata_fieldset.js
@@ -0,0 +1,23 @@
+/**
+ * @file custom_module.js
+ * Custom Module main JavaScript file.
+ *
+ * You will need to get rid of the comments.
+ * For how to add this file to your module, see https://drupal.org/node/304255
+ */
+
+(function ($) {
+
+  Drupal.behaviors.open_metadata_fieldset = {
+
+    attach: function (context, settings) {
+
+      console.log('Hello world');
+
+	  $('fieldset.islandora-metadata', context).removeClass( "collapsed" );
+      //$('fieldset.islandora-metadata .fieldset-legend', context).unwrap().wrap( '<h2 class="metadata label"></h2>' );
+      //$('fieldset.islandora-metadata .fieldset-wrapper', context).unwrap().addClass( "metadata" );
+
+    }
+  };
+})(jQuery);
